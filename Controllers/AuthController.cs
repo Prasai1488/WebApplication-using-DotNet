f2c99fc8b4321c4685cb6b72973b88f15627c087
Prasai1488 <prasaiprithvi4@gmail.com>
2024-12-06 21:34:59 +0545

    login api

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -65,6 +65,33 @@ namespace WebApplication1.Controllers
             }
         }
 
+        // Login API
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDto)
+        {
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginDto.Username);
+
+                if (user == null)
+                
+                    return Unauthorized(new { Message = "Invalid credentials" });
+
+                if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+                    return Unauthorized(new { Message = "Invalid credentials" });
+
+                // Now lets generate jwt token
+                var token = _jwtService.GenerateJwtToken(user);
+
+                return Ok(new {Message = "Login Success", Token = token });
+
+            } 
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Something unusal happened " });
+            }
+        }
+
     }
 
    
